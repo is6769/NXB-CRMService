152b1c4536b0efe24f119eae654623cc8bb346d9
is6769 <igor.shvets2007@mail.ru>
2025-05-06 17:17:23 +0300

    first successful version of project

--- src/main/java/org/example/crmservice/filters/HeadersFilter.java
+++ src/main/java/org/example/crmservice/filters/HeadersFilter.java
@@ -3,6 +3,8 @@ package org.example.crmservice.filters;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
@@ -11,19 +13,22 @@ import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.context.SecurityContextImpl;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Objects;
 
 @Component
-public class HeadersFilter implements Filter {
+public class HeadersFilter extends OncePerRequestFilter {
+    private static final Logger log = LoggerFactory.getLogger(HeadersFilter.class);
+
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        HttpServletRequest httpRequest = (HttpServletRequest) request;
-        String userId = httpRequest.getHeader("X-User-Id");
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        log.info("HEADERS FILTER TRIGGERED");
+        String userId = request.getHeader("X-User-Id");
         if (Objects.nonNull(userId)){
-            GrantedAuthority role = new SimpleGrantedAuthority(httpRequest.getHeader("role"));
+            GrantedAuthority role = new SimpleGrantedAuthority(request.getHeader("X-User-Role"));
 
             //if (userId.equals("MANAGER"))
             Authentication authentication = new UsernamePasswordAuthenticationToken(
@@ -31,10 +36,11 @@ public class HeadersFilter implements Filter {
                     null,
                     List.of(role)
             );
+            log.info(authentication.toString());
             SecurityContext securityContext = new SecurityContextImpl(authentication);
             SecurityContextHolder.setContext(securityContext);
             //}
         }
-        chain.doFilter(request, response);
+        filterChain.doFilter(request, response);
     }
 }
