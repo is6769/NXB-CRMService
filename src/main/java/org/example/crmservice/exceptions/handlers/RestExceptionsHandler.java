7e994633382fbd7fa4df1819d519cf94ff5567af
is6769 <igor.shvets2007@mail.ru>
2025-05-12 08:59:46 +0300

    added javadoc to crm service

--- src/main/java/org/example/crmservice/exceptions/handlers/RestExceptionsHandler.java
+++ src/main/java/org/example/crmservice/exceptions/handlers/RestExceptionsHandler.java
@@ -14,10 +14,19 @@ import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.HttpStatusCodeException;
 
-
+/**
+ * Глобальный обработчик исключений для REST контроллеров.
+ * Перехватывает специфичные исключения и возвращает стандартизированные HTTP ответы.
+ */
 @RestControllerAdvice
 public class RestExceptionsHandler {
 
+    /**
+     * Обрабатывает исключения типа {@link HttpClientErrorException}, возникающие при клиентских ошибках (4xx).
+     * Возвращает ответ с тем же HTTP статусом, телом и заголовками, что и в исходном исключении.
+     * @param ex Исключение {@link HttpClientErrorException}.
+     * @return {@link ResponseEntity} с деталями ошибки.
+     */
     @ExceptionHandler(exception = {HttpClientErrorException.class, HttpServerErrorException.class})
     public ResponseEntity<byte[]> handleHttpClientExceptions(HttpStatusCodeException ex){
         HttpHeaders originalHeaders = ex.getResponseHeaders();
@@ -40,6 +49,12 @@ public class RestExceptionsHandler {
                 .body(ex.getResponseBodyAsByteArray());
     }
 
+    /**
+     * Обрабатывает исключения типа {@link MethodArgumentNotValidException}, возникающие при ошибках валидации аргументов метода.
+     * Возвращает ответ со статусом BAD_REQUEST и {@link ExceptionDTO}, содержащим информацию об ошибках валидации.
+     * @param ex Исключение {@link MethodArgumentNotValidException}.
+     * @return {@link ResponseEntity} с {@link ExceptionDTO}, описывающим ошибки валидации.
+     */
     @ExceptionHandler(exception = MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ExceptionDTO handleSubscriberAlreadyExistsException(HttpServletRequest request, Exception ex){
